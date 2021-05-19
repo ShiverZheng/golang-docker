7451f6d3ef425df828a74cbcb808a4bb5aabe507
shiverzheng <zhengxiwen@funlearnworld.com>
2021-05-19 14:25:05 +0800

    server mux

--- src/main.go
+++ src/main.go
@@ -1,24 +1,79 @@
 package main
 
 import (
-"fmt"
-"net/http"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-func main() {
-	http.HandleFunc("/", hello)
-	server := &http.Server{
-		Addr: ":8080",
-	}
-	fmt.Println("server startup...")
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Printf("server startup failed, err:%v\n", err)
-	}
+type fileHandler struct {
+	root    http.FileSystem
+	handler http.Handler
 }
 
-func hello(w http.ResponseWriter, _ *http.Request) {
+func fileServer(root http.FileSystem, h http.Handler) http.Handler {
+	return &fileHandler{root, h}
+}
+
+type indexHandler struct {
+}
+
+func (i *indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte("awesome docker!"))
 	if err != nil {
 		fmt.Printf("something error ...")
+	}
+}
+
+func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	if _, err := os.Stat("./static" + path); os.IsNotExist(err) {
+		if err != nil {
+			fmt.Printf("something error ...")
+		}
+		return
+	}
+	f.handler.ServeHTTP(w, r)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	staticPath := http.Dir("static")
+	f := fileServer(&staticPath, http.FileServer(staticPath))
+	mux.Handle("/", &indexHandler{})
+	mux.Handle("/jd", f)
+	mux.Handle("/pinyou", f)
+	mux.Handle("/pokemon", f)
+	mux.Handle("/suning", f)
+	mux.Handle("/ctrip", f)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// 创建系统信号接收器
+	done := make(chan os.Signal)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-done
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatal("Shutdown server:", err)
+		}
+	}()
+
+	log.Println("Starting HTTP server...")
+	err := server.ListenAndServe()
+	if err != nil {
+		if err == http.ErrServerClosed {
+			log.Print("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpected")
+		}
 	}
 }
